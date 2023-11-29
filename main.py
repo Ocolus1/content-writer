90bebe56cb43e7b9aeb946fb527341588f213b60
Paul <awolesiboluwatife@gmail.com>
2023-11-29 16:09:04 +0100

    modified the prompt

--- main.py
+++ main.py
@@ -180,7 +180,9 @@ def generate_prompt():
 
     # New Enhancements
     prompt += " Include real-world case studies or examples for practical insights if relevant."
-    prompt += " Customize the introduction and conclusion to add uniqueness, make it lengthy."
+    prompt += (
+        " Customize the introduction and conclusion to add uniqueness, make it lengthy."
+    )
     prompt += " Tailor the content to the specified target audience if relevant."
     prompt += " Suggest interactive elements like polls or quizzes if relevant."
     prompt += " Recommend social media post ideas related to the article if relevant."
@@ -189,37 +191,175 @@ def generate_prompt():
     return prompt
 
 
+def generate_title():
+    title = f"Generate a single line blog title focused on {st.session_state['target_keyword']}, ensuring it is engaging, SEO-optimized, and reflective of key themes in {st.session_state['target_keyword']}."
+    return title
+
+
+def generate_intro(title):
+    intro = f"""
+    Write an introduction for the article titled '{title}', 
+    setting the stage for an in-depth exploration of {st.session_state['target_keyword']}, 
+    its importance in the relevant field, and its impact on users or industry trends
+    """
+    return intro
+
+
+def generate_body():
+    body = f"""
+    Discuss the fundamental principles or basics of {st.session_state['target_keyword']}, and their relevance in the current context.
+    Explore advanced concepts, trends, or strategies related to {st.session_state['target_keyword']} and how they can be applied effectively.
+    Share practical tips, insights, or case studies illustrating the successful application of {st.session_state['target_keyword']}.
+    """
+    return body
+
+
+def generate_FAQ():
+    faq = f"""
+        Create a FAQ section addressing common questions and misconceptions about {st.session_state['target_keyword']} providing clear and concise answers.
+    """
+    return faq
+
+
+def generate_youtube():
+    youtube = f"""
+        List YouTube resources, such as channels or videos, offering valuable insights, tutorials, or case studies on {st.session_state['target_keyword']} in  URL links.
+    """
+    return youtube
+
+
+def generate_metadata():
+    metadata = f"""
+        Draft a compelling meta description for the article, summarizing its key points about {st.session_state['target_keyword']} and inviting readers to explore in-depth
+    """
+    return metadata
+
+
+def generate_image():
+    img = f"""
+        Give  an ideal featured image for the article that visually represents the core themes or concepts of {st.session_state['target_keyword']} in a URL link.
+    """
+    return img
+
+
+# def generate_prompts():
+#     prompt = ""
+
+#     intro = f""
+#     pass
+
+
+# Assuming you have a list to track the conversation history
+conversation_history = []
+
+
 def get_gpt4_response(prompt):
+    # Add the new user prompt to the conversation history
+    conversation_history.append({"role": "user", "content": prompt})
+
+    # Prepare the messages for the API call, including past messages
+    messages = []
+    for message in conversation_history[
+        -14:
+    ]:  # Include the last 10 interactions, for example
+        messages.append(message)
+
+    # GPT-4 API call
     completion = client.chat.completions.create(
         model="gpt-4",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a talented writer, a skilled professional in detailed, lengthy and robust writing skills.",
-            },
-            {
-                "role": "user",
-                "content": prompt,
-            },
-        ],
+        messages=messages,
         max_tokens=4096,
     )
 
-    return completion.choices[0].message.content
+    # Get the response and add it to the conversation history
+    response = completion.choices[0].message.content
+    conversation_history.append({"role": "system", "content": response})
+
+
+    return response
+
+
+# Function to update the progress bar and the status text
+def update_progress(progress_bar, status_text, step, total_steps):
+    progress = step / total_steps
+    progress_bar.progress(progress)
+    status_text.text(f"Generating content: Step {step}/{total_steps}...")
 
 
 def display_article():
     st.header("Generated Article")
 
-    # Generate the prompt
-    prompt = generate_prompt()
+    # Total number of steps in the content generation process
+    total_steps = 7
+
+    # Initialize the progress bar and status text
+    progress_bar = st.progress(0)
+    status_text = st.empty()
 
     # Show a spinner while waiting for the response
     with st.spinner("Generating article, please wait..."):
-        generated_article = get_gpt4_response(prompt)
+        # Step 1: Generate Title
+        update_progress(progress_bar, status_text, 1, total_steps)
+        get_title = generate_title()
+        generated_title = get_gpt4_response(get_title)
+
+
+        # Step 2: Generate Intro
+        update_progress(progress_bar, status_text, 2, total_steps)
+        get_intro = generate_intro(generated_title)
+        generated_intro = get_gpt4_response(get_intro)
+
+
+        # Step 3: Generate Body
+        update_progress(progress_bar, status_text, 3, total_steps)
+        get_body = generate_body()
+        generated_body = get_gpt4_response(get_body)
+
+
+        # Step 4: Generate FAQ
+        update_progress(progress_bar, status_text, 4, total_steps)
+        get_faq = generate_FAQ()
+        generated_faq = get_gpt4_response(get_faq)
+
+
+        # Step 5: Generate YouTube Suggestions
+        update_progress(progress_bar, status_text, 5, total_steps)
+        get_youtube = generate_youtube()
+        generated_youtube = get_gpt4_response(get_youtube)
+
+
+        # Step 6: Generate Metadata
+        update_progress(progress_bar, status_text, 6, total_steps)
+        get_metadata = generate_metadata()
+        generated_metadata = get_gpt4_response(get_metadata)
+
+        # Step 7: Generate Image Suggestions
+        update_progress(progress_bar, status_text, 7, total_steps)
+        get_image = generate_image()
+        generated_image = get_gpt4_response(get_image)
+
+    # Update the status to indicate completion
+    status_text.text("Content generation complete!")
 
     # Display the article
-    st.text_area("Article", generated_article, height=500)
+    generated_article = f"""
+{generated_title[1:-1]}
+
+{generated_intro}
+
+{generated_body}
+
+{generated_faq}
+
+{generated_youtube}
+
+{generated_metadata}
+
+{generated_image}
+
+    """
+    st.text_area("Article", generated_article, height=600)
+    conversation_history.clear()
 
 
 # Run the app
